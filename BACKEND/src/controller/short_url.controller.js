f1aaf1a65d6a5aed4b0567b73010e7736c9a3aa2
kunalsingh001 <ks163484@gmail.com>
2026-02-05 19:01:47 +0530

    Add backend and frontend

--- BACKEND/src/controller/short_url.controller.js
+++ BACKEND/src/controller/short_url.controller.js
@@ -1,28 +1,50 @@
-import { getShortUrl } from "../dao/short_url.js"
-import { createShortUrlWithoutUser, createShortUrlWithUser } from "../services/short_url.service.js"
-import wrapAsync from "../utils/tryCatchWrapper.js"
+import { getShortUrl } from "../dao/short_url.js";
+import {
+  createShortUrlWithoutUser,
+  createShortUrlWithUser,
+} from "../services/short_url.service.js";
+import wrapAsync from "../utils/tryCatchWrapper.js";
 
-export const createShortUrl = wrapAsync(async (req,res)=>{
-    const data = req.body
-    let shortUrl
-    if(req.user){
-        shortUrl = await createShortUrlWithUser(data.url,req.user._id,data.slug)
-    }else{  
-        shortUrl = await createShortUrlWithoutUser(data.url)
-    }
-    res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
-})
+// CREATE SHORT URL
+export const createShortUrl = wrapAsync(async (req, res) => {
+  const data = req.body;
+  let shortUrl;
 
+  if (req.user) {
+    shortUrl = await createShortUrlWithUser(
+      data.url,
+      req.user._id,
+      data.slug
+    );
+  } else {
+    shortUrl = await createShortUrlWithoutUser(data.url);
+  }
 
-export const redirectFromShortUrl = wrapAsync(async (req,res)=>{
-    const {id} = req.params
-    const url = await getShortUrl(id)
-    if(!url) throw new Error("Short URL not found")
-    res.redirect(url.full_url)
-})
+  res.status(200).json({
+    shortUrl: `${process.env.APP_URL}/${shortUrl}`,
+  });
+});
 
-export const createCustomShortUrl = wrapAsync(async (req,res)=>{
-    const {url,slug} = req.body
-    const shortUrl = await createShortUrlWithoutUser(url,customUrl)
-    res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
-})
+// REDIRECT FROM SHORT URL
+export const redirectFromShortUrl = wrapAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const url = await getShortUrl(id);
+
+  if (!url) {
+    return res.status(404).json({ message: "Short URL not found" });
+  }
+
+  return res.redirect(url.full_url);
+});
+
+// CREATE CUSTOM SHORT URL
+export const createCustomShortUrl = wrapAsync(async (req, res) => {
+  const { url, slug } = req.body;
+
+  const shortUrl = await createShortUrlWithoutUser(url, slug);
+
+  res.status(200).json({
+    shortUrl: `${process.env.APP_URL}/${shortUrl}`,
+  });
+});
--- FRONTEND/src/components/UserUrl.jsx
+++ FRONTEND/src/components/UserUrl.jsx
@@ -85,7 +85,7 @@ const UserUrl = () => {
                       rel="noopener noreferrer"
                       className="text-blue-600 hover:text-blue-900 hover:underline"
                     >
-                      {`localhost:3000/${url.short_url}`}
+                      {`http://localhost:3000/${url.short_url}`}
                     </a>
                   </div>
                 </td>
